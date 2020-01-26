fb7506ab2ecf78a37b6204187126766c87d03a3a
Kasaract <garynguyen2018@gmail.com>
2020-01-26 08:24:53 -0500

    Updates

---
+++ precache-manifest.b3a98f39572e72f1e3de3b346a521492.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda4d6a8033f7d7082511eb28edbc811",
+    "url": "/deploy-matchr-proto/index.html"
+  },
+  {
+    "revision": "fe412776585fc85cb9d0",
+    "url": "/deploy-matchr-proto/static/css/main.55c853ec.chunk.css"
+  },
+  {
+    "revision": "b574a2cd793cb7f7c2ad",
+    "url": "/deploy-matchr-proto/static/js/2.da3bdaa3.chunk.js"
+  },
+  {
+    "revision": "4fbb664bfbe1cf0e394a9cfc5137f9b6",
+    "url": "/deploy-matchr-proto/static/js/2.da3bdaa3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe412776585fc85cb9d0",
+    "url": "/deploy-matchr-proto/static/js/main.dc41ad72.chunk.js"
+  },
+  {
+    "revision": "09ab9270cd3810ec4156",
+    "url": "/deploy-matchr-proto/static/js/runtime-main.2f0130a3.js"
+  },
+  {
+    "revision": "62c92389b7f5cb64cdea6fd18babdc71",
+    "url": "/deploy-matchr-proto/static/media/headshot.62c92389.jpg"
+  },
+  {
+    "revision": "ce49a946057f44e7c3a8fd474aa993f9",
+    "url": "/deploy-matchr-proto/static/media/spotify.ce49a946.svg"
+  }
+]);
